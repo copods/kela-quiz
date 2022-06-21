d78c31ef3a3e1e38a1271d8430ec4170857954fe
Anurag <anurag@copods.co>
2022-06-21 14:19:55 +0530

    .gitIgnore added tailwind.css

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -2,9 +2,6 @@ import type { LoaderFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 
 import { requireUserId } from "~/session.server";
-import { Icon } from '@iconify/react';
-
-
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await requireUserId(request);
