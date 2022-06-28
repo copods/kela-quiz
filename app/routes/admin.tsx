63ece474e048e155a31509a6ba591ded533e17d0
Shreya <shreyajain@copods.co>
2022-06-28 13:57:46 +0530

    Removed extra comments

--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,20 +1,17 @@
 import { Outlet } from "@remix-run/react";
-import { LoaderFunction } from "@remix-run/server-runtime";
+import type { LoaderFunction } from "@remix-run/server-runtime";
 import SideNav from "~/components/side-nav/SideNav";
 
 import { requireUserId } from "~/session.server";
 
 
 export const loader: LoaderFunction = async ({ request }) => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const userId = await requireUserId(request);
   return null
 };
 
 
 export default function NotesPage() {
-  // const user= useUser()
-  // console.log(user)
   return (
     <main className="flex min-h-screen">
         <div className="w-2/12 bg-white min-w-260 p-5 drop-shadow-md">
--- app/routes/admin/dashboard/index.tsx
+++ app/routes/admin/dashboard/index.tsx
@@ -2,6 +2,5 @@ import { useOptionalUser } from "~/utils"
 
 export default function Dashboard() {
   const user = useOptionalUser();
-
   return <div>Dashboard Works...!</div>
 }
