22cb2b5804cccf02a04f604b9b8c0c656ad95907
Anurag <anurag@copods.co>
2022-07-01 16:57:36 +0530

    snackbar

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -2,6 +2,7 @@ import AdminLayout from "~/components/layouts/AdminLayout";
 import { getUserId } from '~/session.server'
 import { redirect } from '@remix-run/node'
 import type { LoaderFunction } from '@remix-run/node'
+import { toast } from 'react-toastify';
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await getUserId(request)
@@ -9,10 +10,21 @@ export const loader: LoaderFunction = async ({ request }) => {
   return null
 }
 
+
 export default function Dashboard() {
+
+  const notify = () => {
+    console.log("hashdkad", toast)
+    toast.warn("Wow so easy!");
+    toast.error("Wow so easy!");
+    toast.success("Wow so easy!");
+  }
   return (
     <AdminLayout>
-      <div>Hey Dashboard</div>
+      <div>
+        <div>Hey Dashboard</div>
+        <button onClick={notify}>Hey</button>
+      </div>
     </AdminLayout>
   )
 }
