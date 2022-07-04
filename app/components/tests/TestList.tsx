811e89ce20767c6c51abf78ecdff9e566e29e1fe
Anurag <anurag@copods.co>
2022-07-04 21:54:15 +0530

    test cases

--- app/components/tests/TestList.tsx
+++ app/components/tests/TestList.tsx
@@ -8,7 +8,7 @@ const TestList = ({ tests }: { tests: Array<Test> }) => {
       <header className="flex justify-between items-center">
         <h2 title="Tests" className="text-3xl font-bold text-black">Tests</h2>
         <Link to={'/tests/add-test'}>
-          <button className="px-5 h-9 text-[#F0FDF4] bg-primary rounded-lg text-xs" >+ Add Test</button>
+          <button id="addTest" className="px-5 h-9 text-[#F0FDF4] bg-primary rounded-lg text-xs" >+ Add Test</button>
         </Link>
       </header>
       <div className="bg-white border border-gray-200 rounded-lg p-6">
---
+++ cypress/e2e/tests/add-test.cy.ts
@@ -0,0 +1,53 @@
+describe('Visiting Add Test Page', () => {
+  it('Check Section', () => {
+    cy.visit('/sign-in')
+    cy.get('#email').clear()
+      .type('careers@copods.co')
+      .should('have.value', 'careers@copods.co')
+    cy.get('#password').clear()
+      .type('kQuiz@copods')
+      .should('have.value', 'kQuiz@copods')
+    cy.findByRole('button').click()
+
+    cy.get('a').find('#Tests').should('have.text', 'Tests').click()
+    cy.location('pathname', { timeout: 60000 }).should('include', '/tests') 
+    cy.get('button').find('#addTest').should('have.text', 'Add Test').click()
+    cy.location('pathname', { timeout: 60000 }).should('include', '/tests/add-test') 
+
+  })
+
+  // it("Add Section modal open and Add section", () =>{
+  //   cy.visit('/sign-in')
+  //   cy.get('#email').clear()
+  //     .type('careers@copods.co')
+  //     .should('have.value', 'careers@copods.co')
+  //   cy.get('#password').clear()
+  //     .type('kQuiz@copods')
+  //     .should('have.value', 'kQuiz@copods')
+  //   cy.findByRole('button').click()
+
+  //   cy.get('a').find('#Sections').should('have.text', 'Sections').click()
+  //   cy.location('pathname', { timeout: 60000 }).should('include', '/sections')
+  //   cy.get("#add-section").click()
+  //   cy.get("#headlessui-dialog-panel-6",{ timeout: 10000 }).should("be.visible")
+  //   cy.get(":nth-child(3) > .h-11").type(`Aptitude - ${new Date().getTime()}`)
+  //   cy.get(":nth-child(4) > .h-11").type("Aptitude")
+  //   cy.get(".justify-end > .bg-primary",{ timeout: 10000 }).click()
+  // })
+
+  // it("Add Section modal open and cancel section", () =>{
+  //   cy.visit('/sign-in')
+  //   cy.get('#email').clear()
+  //     .type('careers@copods.co')
+  //     .should('have.value', 'careers@copods.co')
+  //   cy.get('#password').clear()
+  //     .type('kQuiz@copods')
+  //     .should('have.value', 'kQuiz@copods')
+  //   cy.findByRole('button').click()
+  //   cy.get('a').find('#Sections').should('have.text', 'Sections').click()
+  //   cy.location('pathname', { timeout: 60000 }).should('include', '/sections')
+  //   cy.get("#add-section").click()
+  //   cy.get("#headlessui-dialog-panel-6",{ timeout: 10000 }).should("be.visible")
+  //   cy.get(".justify-end > .text-gray-500",{ timeout: 10000 }).click()
+  // })
+})
