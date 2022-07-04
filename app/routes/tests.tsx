7b23ce45492f7a99715366c482011ddaa7fd5591
Anurag <anurag@copods.co>
2022-07-04 14:45:27 +0530

    try catch added

--- app/routes/tests.tsx
+++ app/routes/tests.tsx
@@ -6,22 +6,38 @@ import TestList from "~/components/tests/TestList";
 import { getAllTests } from "~/models/tests.server";
 import { json } from '@remix-run/node'
 import { useLoaderData } from "@remix-run/react";
+import type { Test, User } from "@prisma/client";
+import { toast } from "react-toastify";
 
 
 type LoaderData = {
   tests: Awaited<ReturnType<typeof getAllTests>>
+  status: string
 }
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await getUserId(request)
   if (!userId) return redirect('/sign-in')
-  const tests = await getAllTests()
-  return json<LoaderData>({ tests })
+  var tests: Array<(Test & { createdBy: User; })> = []
+  var status: string = ''
+  await getAllTests()
+    .then(res => {
+      toast.success("hey")
+      tests = res
+      status = "Success"
+    }).catch(err => {
+      console.log(err)
+      status = err
+    })
+  return json<LoaderData>({ tests, status })
 }
 
 
 export default function Results() {
   const data = useLoaderData() as LoaderData
+  if (data.status != "Success") {
+    toast.success("Something went wrong..!")
+  }
   return (
     <AdminLayout>
       <TestList tests={data.tests} />
