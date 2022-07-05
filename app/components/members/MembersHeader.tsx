6e20f863a768169b866386ab3a694717c8bb1ab7
Anurag <anurag@copods.co>
2022-07-05 15:08:40 +0530

    comment fixes

--- app/components/members/MembersHeader.tsx
+++ app/components/members/MembersHeader.tsx
@@ -1,20 +1,20 @@
-import type{ Role } from "@prisma/client"
+import type { Role } from "@prisma/client"
 import { useState } from "react"
-import AddMembers from "./AddMembers"
+import AddMember from "./AddMember"
 
-export  default function MembersHeader({roles}: {roles: Role[]}){
+export default function MembersHeader({ roles }: { roles: Role[] }) {
   const [addMemberModal, setAddMemberModalValue] = useState(false)
-  return(
+  return (
     <div className="grid grid-cols-12 pb-12">
-       <div className="col-span-full grid">
-            <div className="flex items-center justify-between">
-              <h1 className="text-3xl font-bold leading-9">Members</h1>
-              <button id="addMember" className="rounded-lg bg-blue-900 px-4  h-9  text-xs font-medium leading-4 text-[#F0FDF4]" onClick={() => setAddMemberModalValue(!addMemberModal)}>
-                + Add Member
-              </button>
-            </div>
-          </div>
-          <AddMembers roles={roles} addMemberModalOpen={addMemberModal} setAddSectionModalOpen={setAddMemberModalValue} />
+      <div className="col-span-full grid">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold leading-9">Members</h1>
+          <button id="addMember" className="rounded-lg bg-blue-900 px-4  h-9  text-xs font-medium leading-4 text-[#F0FDF4]" onClick={() => setAddMemberModalValue(!addMemberModal)}>
+            + Add Member
+          </button>
+        </div>
+      </div>
+      <AddMember roles={roles} addMemberModalOpen={addMemberModal} setAddMemberModalOpen={setAddMemberModalValue} />
     </div>
   )
 }
