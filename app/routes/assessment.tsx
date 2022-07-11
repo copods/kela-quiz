73e0efcbe2407a1c0300b4a2e11398c308430fce
Anurag <anurag@copods.co>
2022-07-11 12:01:09 +0530

    initial

---
+++ app/routes/assessment.tsx
@@ -0,0 +1,12 @@
+import AdminLayout from "~/components/layouts/AdminLayout";
+
+export default function Dashboard() {
+
+  return (
+    <AdminLayout>
+      <div>
+        <div>Assessment Works.....!</div>
+      </div>
+    </AdminLayout>
+  )
+}
