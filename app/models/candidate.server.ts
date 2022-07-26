22d25e4ed33151d1d99b563f290a1f4619370332
Anurag <anurag@copods.co>
2022-07-26 11:25:19 +0530

    init

--- app/models/candidate.server.ts
+++ app/models/candidate.server.ts
@@ -2,95 +2,131 @@ import type { User } from '@prisma/client'
 import { env } from 'process'
 import { prisma } from '~/db.server'
 
+export async function createIndividualCandidate({ email, createdById }: { email: string, createdById: string }) {
+  try {
+    // checkig if candidate already exists
+    let user = await prisma.candidate.findUnique({ where: { email } })
+
+    // if candidate doesnot exist in database, create one
+    if (!user) {
+      user = await prisma.candidate.create({
+        data: {
+          email,
+          createdById
+        },
+      })
+    }
+    return user
+  } catch (error) {
+    console.log(error)
+    throw new Error('Candidate Create Error..!')
+  }
+}
+
+export async function createCandidateTest({ testId, candidateId }: { testId: string, candidateId: string }) {
+  try {
+    // creating test for candidate
+    let candidateTest = prisma.candidateTest.create({
+      data: {
+        testId,
+        candidateId
+      }
+    })
+    return candidateTest
+  } catch (error) {
+    console.log(error)
+    throw new Error('Candidate Test Create Error..!')
+  }
+}
+
+export async function updateTestLink({ id, link }: { id: string, link: string }) {
+  try {
+    prisma.candidateTest.update({
+      where: {
+
+      },
+      data: {
+        link
+      },
+    })
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error updating test link..!')
+  }
+}
+
 async function createCandidateData({
   email,
   createdById,
   testId
 }: { email: string, createdById: User['id'], testId: string }) {
 
-  // checkig if candidate already exists
-  let user = await prisma.candidate.findUnique({ where: { email } })
+  let user = await createIndividualCandidate({ email, createdById })
 
-  // if candidate doesnot exist in database, create one
-  if (!user) {
-    user = await prisma.candidate.create({
-      data: {
-        email,
-        createdById
-      },
-    })
-  }
-  console.log("user", user)
+  let candidateTest = await createCandidateTest({ testId, candidateId: user.id })
 
-  // creating test for candidate
-  prisma.candidateTest.create({
+  console.log(user)
+  console.log(candidateTest)
+
+
+
+  // generating random link
+  const candidateLink = env.PUBLIC_URL + '/assesment/' + candidateTest.id
+
+  // storing link to db
+  prisma.candidateTest.update({
+    where: {
+      id: candidateTest.id
+    },
     data: {
-      testId,
-      candidateId: user.id,
-    }
-  }).then(candidateTest => {
-    console.log(candidateTest)
+      link: candidateLink,
+    },
+  }).then(() => {
 
-    // generating random link
-    const candidateLink = env.PUBLIC_URL + '/assesment/' + candidateTest.id
+    // get test for which user should be registered for
+    prisma.test.findUnique({
+      where: { id: testId },
+      include: { sections: true },
+    }).then(test => {
+      console.log("test: ", test)
+      // creating section in test
+      if (test?.sections) {
+        for (let section of test.sections) {
+          prisma.sectionInCandidateTest.create({
+            data: {
+              sectionId: section.sectionId,
+              candidateTestId: candidateTest.id,
+            }
+          }).then(sectionInCandidateTest => {
+            console.log('section in candidatetest: ', sectionInCandidateTest)
 
-    // storing link to db
-    prisma.candidateTest.update({
-      where: {
-        id: candidateTest.id
-      },
-      data: {
-        link: candidateLink,
-      },
-    }).then(() => {
+            prisma.$queryRaw`SELECT * FROM "Question" WHERE "sectionId" = ${section.sectionId} ORDER BY RANDOM() LIMIT ${section.totalQuestions};`.then((randomQuestions: any) => {
+              console.log("randomQu:", randomQuestions)
+              for (let question of randomQuestions) {
+                prisma.candidateQuestion.create({
+                  data: {
+                    questionId: question.id,
+                    sectionInCandidateTestId: sectionInCandidateTest.id
+                  }
+                }).then(question => {
 
-      // get test for which user should be registered for
-      prisma.test.findUnique({
-        where: { id: testId },
-        include: { sections: true },
-      }).then(test => {
-        console.log("test: ", test)
-        // creating section in test
-        if (test?.sections) {
-          for (let section of test.sections) {
-            prisma.sectionInCandidateTest.create({
-              data: {
-                sectionId: section.sectionId,
-                candidateTestId: candidateTest.id,
+                }).catch(err => {
+
+                })
               }
-            }).then(sectionInCandidateTest => {
-              console.log('section in candidatetest: ', sectionInCandidateTest)
-
-              prisma.$queryRaw`SELECT * FROM "Question" WHERE "sectionId" = ${section.sectionId} ORDER BY RANDOM() LIMIT ${section.totalQuestions};`.then((randomQuestions: any) => {
-                console.log("randomQu:", randomQuestions)
-                for (let question of randomQuestions) {
-                  prisma.candidateQuestion.create({
-                    data: {
-                      questionId: question.id,
-                      sectionInCandidateTestId: sectionInCandidateTest.id
-                    }
-                  }).then(question => {
-
-                  }).catch(err => {
-
-                  })
-                }
-              }).catch(err => {
-                return { err }
-              })
             }).catch(err => {
               return { err }
             })
-          }
+          }).catch(err => {
+            return { err }
+          })
         }
-
-      }).catch(err => {
-        return { err }
-      })
+      }
 
     }).catch(err => {
       return { err }
     })
+
 
   }).catch(err => {
     return { err }
