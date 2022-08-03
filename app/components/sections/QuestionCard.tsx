e279d4207615aa95795197b9d19e9c93f53baa9c
yash thakur <shivansh@copods.co>
2022-08-03 14:40:08 +0530

    QuestionblockClick

--- app/components/sections/QuestionCard.tsx
+++ app/components/sections/QuestionCard.tsx
@@ -20,23 +20,45 @@ const QuestionCard = ({
       key={question.id}
       className="flex flex-col  rounded-lg border border-gray-300 bg-gray-50 px-6 py-7"
     >
-      <div className="flex items-center justify-between overflow-ellipsis break-all text-base text-gray-600">
-        <div className="ql-editor flex-1">
-          <div dangerouslySetInnerHTML={{ __html: question.question }}></div>
-        </div>
+      <div className="items-top flex  justify-between overflow-ellipsis break-all text-base text-gray-600">
+        {/* <div className="ql-editor flex-1 ">
+          <div
+            className="cursor-pointer"
+            onClick={() => onAccordianToggle(index)}
+            dangerouslySetInnerHTML={{ __html: question.question }}
+          ></div>
+        </div> */}
 
         {isExpanded === index ? (
-          <Icon
-            icon={'akar-icons:circle-chevron-up'}
-            className="cursor-pointer text-xl text-gray-400"
-            onClick={() => onAccordianToggle(-1)}
-          />
+          <div className="items-top flex flex-1 justify-between">
+            <div className="ql-editor flex-1 ">
+              <div
+                className="cursor-pointer"
+                onClick={() => onAccordianToggle(-1)}
+                dangerouslySetInnerHTML={{ __html: question.question }}
+              ></div>
+            </div>
+            <Icon
+              icon={'akar-icons:circle-chevron-up'}
+              className="mt-3 cursor-pointer text-xl text-gray-400"
+              onClick={() => onAccordianToggle(-1)}
+            />
+          </div>
         ) : (
-          <Icon
-            icon={'akar-icons:circle-chevron-down'}
-            className="cursor-pointer text-xl text-gray-400"
-            onClick={() => onAccordianToggle(index)}
-          />
+          <div className="items-top flex flex-1 justify-between">
+            <div className="ql-editor flex-1 ">
+              <div
+                className="cursor-pointer"
+                onClick={() => onAccordianToggle(index)}
+                dangerouslySetInnerHTML={{ __html: question.question }}
+              ></div>
+            </div>
+            <Icon
+              icon={'akar-icons:circle-chevron-down'}
+              className="mt-3 cursor-pointer text-xl text-gray-400"
+              onClick={() => onAccordianToggle(index)}
+            />
+          </div>
         )}
       </div>
       <div
