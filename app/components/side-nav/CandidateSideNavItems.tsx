8f0530eb56cd9cd15ef109d1ba883ab7110ee882
yash thakur <shivansh@copods.co>
2022-08-06 11:27:43 +0530

    sideNav

--- app/components/side-nav/CandidateSideNavItems.tsx
+++ app/components/side-nav/CandidateSideNavItems.tsx
@@ -14,28 +14,41 @@ const CandidateSideNavItems = ({
       <div className="">
         <div className="flex items-center justify-between py-3 text-sm font-medium leading-5 text-gray-800 ">
           Section 1
-          {isExpanded === index ? (
-            <Icon
-              icon={'bytesize:lock'}
-              className="cursor-pointer text-xl text-gray-800"
-              onClick={() => onAccordianToggle(-1)}
-            />
-          ) : (
-            <Icon
-              icon={'teenyicons:tick-circle-outline'}
-              className="cursor-pointer text-xl text-green-800"
-              onClick={() => onAccordianToggle(index)}
-            />
-          )}
+          <div
+            className="items-top flex flex-1 justify-between"
+            onClick={() => {
+              onAccordianToggle(
+                isExpanded == -1 ? index : isExpanded == index ? -1 : index
+              )
+            }}
+          >
+            <div className="ql-editor flex-1 p-0 pr-4">
+              <div
+                className="cursor-pointer"
+                // dangerouslySetInnerHTML={{ __html: question.question }}
+              ></div>
+            </div>
+            {isExpanded === index ? (
+              <Icon
+                icon={'bytesize:lock'}
+                className="cursor-pointer text-xl text-gray-800"
+              />
+            ) : (
+              <Icon
+                icon={'teenyicons:tick-circle-outline'}
+                className="cursor-pointer text-xl text-green-800"
+              />
+            )}
+          </div>
         </div>
-        <div
-          className={
-            'overflow-hidden  text-sm leading-5 text-gray-400 transition-all ' +
-            (isExpanded === index ? 'max-h-96' : 'max-h-0')
-          }
-        >
-          <p className="p-3 text-sm leading-5 text-gray-400">Question</p>
-        </div>
+      </div>
+      <div
+        className={
+          'overflow-hidden   transition-all ' +
+          (isExpanded === index ? 'max-h-96' : 'max-h-0')
+        }
+      >
+        <p className="p-3 text-sm leading-5  text-gray-600">Question</p>
       </div>
     </div>
   )
