ce16983ac462cf6d9a986c0935147abb782036f0
yash thakur <shivansh@copods.co>
2022-08-06 12:42:23 +0530

    fix:link

--- app/components/side-nav/CandidateSideNavItems.tsx
+++ app/components/side-nav/CandidateSideNavItems.tsx
@@ -11,35 +11,29 @@ const CandidateSideNavItems = ({
 }) => {
   return (
     <div>
-      <div className="">
-        <div className="flex items-center justify-between py-3 text-sm font-medium leading-5 text-gray-800 ">
-          Section 1
-          <div
-            className="items-top flex flex-1 justify-between"
-            onClick={() => {
-              onAccordianToggle(
-                isExpanded == -1 ? index : isExpanded == index ? -1 : index
-              )
-            }}
-          >
-            <div className="ql-editor flex-1 p-0 pr-4">
-              <div
-                className="cursor-pointer"
-                // dangerouslySetInnerHTML={{ __html: question.question }}
-              ></div>
-            </div>
-            {isExpanded === index ? (
-              <Icon
-                icon={'bytesize:lock'}
-                className="cursor-pointer text-xl text-gray-800"
-              />
-            ) : (
-              <Icon
-                icon={'teenyicons:tick-circle-outline'}
-                className="cursor-pointer text-xl text-green-800"
-              />
-            )}
+      <div className="flex items-center justify-between py-3 text-sm font-medium leading-5 text-gray-800 ">
+        <div
+          className="items-top flex flex-1 justify-between"
+          onClick={() => {
+            onAccordianToggle(
+              isExpanded == -1 ? index : isExpanded == index ? -1 : index
+            )
+          }}
+        >
+          <div className="ql-editor flex-1 p-0 pr-4">
+            <div className="cursor-pointer"> Section 1</div>
           </div>
+          {isExpanded === index ? (
+            <Icon
+              icon={'bytesize:lock'}
+              className="cursor-pointer text-xl text-gray-800"
+            />
+          ) : (
+            <Icon
+              icon={'teenyicons:tick-circle-outline'}
+              className="cursor-pointer text-xl text-green-800"
+            />
+          )}
         </div>
       </div>
       <div
