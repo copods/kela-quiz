4c6043ce9c149867cb1eec96d737c18792a19b74
Copods <copods@Richa-Bhardwaj-Copods.local>
2022-08-08 14:21:10 +0530

    [WIP]: Work around to find the issue that is failing test cases

--- cypress/e2e/section-details-test/SectionDetails.cy.ts
+++ cypress/e2e/section-details-test/SectionDetails.cy.ts
@@ -29,7 +29,7 @@ describe('Test for Section Details', () => {
     cy.get('a').find('#Sections').should('have.text', 'Sections').click()
     cy.location('pathname', { timeout: 60000 }).should('include', '/sections')
     // cy.get('.px-5').should('contain', '+ Add Section' ).click()
-    cy.get('.px-5').should('be.visible')
+    cy.get('.px-5', { timeout: 10000 }).should('be.visible')
     cy.get('.px-5').click()
     cy.get('.addSectionDilog', { timeout: 10000 }).should('be.visible')
     cy.get('input#sectionName').type(`Aptitude - ${time}`)
