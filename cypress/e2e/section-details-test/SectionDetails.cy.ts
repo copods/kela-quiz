22ef2e28aeea22c02e43cf6cbb72ef121c547f88
Copods <copods@Richa-Bhardwaj-Copods.local>
2022-08-08 13:03:02 +0530

    [WIP]: work around to check why test cases are failing on CI

--- cypress/e2e/section-details-test/SectionDetails.cy.ts
+++ cypress/e2e/section-details-test/SectionDetails.cy.ts
@@ -28,7 +28,10 @@ describe('Test for Section Details', () => {
     cy.findByRole('button').click()
     cy.get('a').find('#Sections').should('have.text', 'Sections').click()
     cy.location('pathname', { timeout: 60000 }).should('include', '/sections')
-    cy.get('.px-5').click()
+    // cy.get('.px-5').should('contain', '+ Add Section' ).click()
+    cy.get('.px-5').should((button) => {
+      expect(button).to.have.contain('+ Add Section')
+    }).click()
     cy.get('.addSectionDilog', { timeout: 10000 }).should('be.visible')
     cy.get('input#sectionName').type(`Aptitude - ${time}`)
     cy.get('textarea#sectionDescription').type(`Aptitude - ${time} Description`)
