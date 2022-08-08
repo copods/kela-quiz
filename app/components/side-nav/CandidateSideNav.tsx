8b92f3b7751bcf37a3f71978ca9d3bb227f98979
yash thakur <shivansh@copods.co>
2022-08-08 09:40:20 +0530

    candidateNavUpdate

--- app/components/side-nav/CandidateSideNav.tsx
+++ app/components/side-nav/CandidateSideNav.tsx
@@ -3,12 +3,13 @@ import CandidateNavFooter from '../CandidateNavFooter'
 import logo from '~/../public/assets/logo.svg'
 import CandidateSideNavItems from './CandidateSideNavItems'
 import { useState } from 'react'
+import { Icon } from '@iconify/react'
 let sideNavGuide = [
   {
     navGuide: 'Assessment Details',
     subItem: [
       {
-        itemName: 'Overview',
+        itemName: 'Section 1',
         // itemRoute: 'Overview',
       },
     ],
@@ -27,12 +28,25 @@ const CandidateSideNav = () => {
         </div>
         <div className="border-t-2-gray-300 border border-t-2 border-x-transparent  border-b-gray-300 px-5 py-5">
           <div className="flex flex-col gap-2 ">
-            <p className="text-xl font-semibold leading-7 text-gray-600">
+            <p className="text-lg font-semibold leading-7 text-gray-600">
               Pre-Interview Assessment
             </p>
-            <p className="text-sm font-medium leading-5 text-gray-500">
-              Time Limit: 30 Mins
-            </p>
+            <div className="flex gap-1">
+              <span>
+                <Icon
+                  icon={'ic:outline-access-time'}
+                  className="cursor-pointer text-xl text-gray-800"
+                />
+              </span>
+              <div className="flex gap-2">
+                <p className="text-sm font-medium leading-5 text-gray-600">
+                  Time Limit:
+                </p>
+                <p className="text-sm font-medium leading-5 text-gray-800">
+                  30 mins
+                </p>
+              </div>
+            </div>
           </div>
         </div>
         <div className="flex flex-col gap-8 px-5 pt-6">
@@ -43,22 +57,18 @@ const CandidateSideNav = () => {
                   {guide.navGuide}
                 </p>
                 <div className="flex flex-col gap-1">
-                  <div className="flex flex-row items-center gap-2">
-                    <div className="flex flex-col gap-1">
-                      {guide.subItem.map((item, index) => {
-                        return (
-                          <div key={index}>
-                            <p
-                              className="non-italic py-3 text-sm font-medium	 leading-5
+                  {guide.subItem.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <span
+                          className="non-italic py-3 text-sm font-medium	 leading-5
                               text-gray-800"
-                            >
-                              {item.itemName}
-                            </p>
-                          </div>
-                        )
-                      })}
-                    </div>
-                  </div>
+                        >
+                          {item.itemName}
+                        </span>
+                      </div>
+                    )
+                  })}
                 </div>
               </div>
             )
