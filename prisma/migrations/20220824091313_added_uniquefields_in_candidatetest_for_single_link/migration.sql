d58ae64692700dbf02cdd78597c4cff76fb4bee8
Anurag <anurag@copods.co>
2022-09-02 13:09:25 +0530

    result details

--- prisma/migrations/20220824091313_added_uniquefields_in_candidatetest_for_single_link/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[candidateId,testId]` on the table `CandidateTest` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "CandidateTest_candidateId_testId_key" ON "CandidateTest"("candidateId", "testId");
