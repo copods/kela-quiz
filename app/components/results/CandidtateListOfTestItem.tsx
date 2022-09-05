172634d62e3613347e34725bc90f5b5fb6fa3add
arpita-20 <arpita@copods.co>
2022-09-05 17:43:22 +0530

    fix(): removed extra spaces

--- app/components/results/CandidtateListOfTestItem.tsx
+++ app/components/results/CandidtateListOfTestItem.tsx
@@ -10,12 +10,12 @@ const CandidtateListOfTestItem = ({
   return (
     <div className="col-span-full grid grid-cols-10">
       <div className="col-span-full grid grid-cols-10 gap-3 border-t border-solid border-gray-200 bg-white px-12 py-6">
-        <div className=" col-span-1 truncate">
-          <span className=" text-base text-gray-700">{index}</span>
+        <div className="col-span-1 truncate">
+          <span className="text-base text-gray-700">{index}</span>
         </div>
         <div className="col-span-3 truncate">{email}</div>
         <div className="col-span-6 truncate">
-          <span className=" text-base text-gray-700">{invitedBy}</span>
+          <span className="text-base text-gray-700">{invitedBy}</span>
         </div>
       </div>
     </div>
