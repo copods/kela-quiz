276e24636577059eb81a354571fc4205cae2d591
DhruvCopods <dhruv.samant+1@copods.co>
2022-09-06 16:37:04 +0530

    db change

---
+++ prisma/migrations/20220906110311_removed_time_in_seconds_key_from_section_wise_result/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `timeInSeconds` on the `SectionWiseResult` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "SectionWiseResult" DROP COLUMN "timeInSeconds";
