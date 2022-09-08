3c466afdf37cfdbb28618a3804f8195cdce44d5a
Anurag <anurag@copods.co>
2022-09-08 11:25:22 +0530

    migration issue fix

--- prisma/migrations/20220906114916_added_unique_constraint_in_section_in_test/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[id,sectionId,testId]` on the table `SectionInTest` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "SectionInTest_id_sectionId_testId_key" ON "SectionInTest"("id", "sectionId", "testId");
