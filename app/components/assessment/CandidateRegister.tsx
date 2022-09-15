f568230b606a77e11ff43ade309d9c0615ffff1f
arpita-20 <arpita@copods.co>
2022-09-15 18:10:45 +0530

    refcator(): removed unnecessary file,removed commented codes,resolved comments

--- app/components/assessment/CandidateRegister.tsx
+++ app/components/assessment/CandidateRegister.tsx
@@ -39,24 +39,14 @@ function CandidateRegister() {
   ]
   return (
     <div className="z-10 flex	 w-full max-w-454 flex-col  gap-12 rounded-lg bg-white p-12 drop-shadow-xl">
-      {/* <div className="z-20 -mt-24 mb-6">
-        <Logo height="64" width="64" />
-      </div> */}
       <h1 className="text-center text-3xl font-bold text-gray-900">Welcome</h1>
       <div className="flex justify-center">
-        {/* <hr className="mt-7 mb-5 h-px w-6/12 border-none bg-gray-500 text-center" /> */}
         <Form method="post" className="flex w-full flex-col gap-10">
           <div className="flex flex-col gap-10">
             {inputFieldsProps.map((props) => {
               return <InputField {...props} key={props.name} />
             })}
           </div>
-          {/* <div className="mt-5 flex flex-row">
-            <Checkbox {...checkBoxProps} />
-            <span className="ml-2 text-xs text-slate-500">
-              {candidateExamConstants.candidateAcceptance}
-            </span>
-          </div> */}
           <Button
             title={commonConstants.proceed}
             buttonText={commonConstants.proceed}
