56fa5a665cf406167f6e53fcda93c0446c493765
NitinCopods <111356197+NitinCopods@users.noreply.github.com>
2022-10-03 10:31:00 +0530

    email layout fixed (#187)
    * email layout fixed
    
    * Update DeletePopUp.tsx
    
    * change
    
    Co-authored-by: anuragP04 <106807559+anuragP04@users.noreply.github.com>
    Co-authored-by: Anurag <anurag@copods.co>

--- app/models/sendgrid.servers.ts
+++ app/models/sendgrid.servers.ts
@@ -11,7 +11,7 @@ export async function sendMail(
   const from = 'careers@copods.co'
   const subject = 'Welcome to K - Quiz @ Copods'
   const text = 'K - Quiz @ Copods'
-
+  const logo = 'K - Quiz logo'
   const html = `<html>
   <head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -19,16 +19,20 @@ export async function sendMail(
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
   </head>
   <body>
-    <div style="min-height:200px;background: url('https://res.cloudinary.com/drpi0krkh/image/upload/v1656596230/User_Invitation_1_he7s2e.png');background-repeat:repeat-x;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;height:50px;text-align:center">
-      <img src="https://res.cloudinary.com/drpi0krkh/image/upload/c_scale,h_200/v1656065336/Logo_jtr0wl.png" alt=K-Quiz Logo height="48" />
-    </div>
+    <div style="min-height:200px;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
+    <div style="background:#353988;padding:15px 0px;text-align:center">
+    <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30"/>
+  </div>
     <div>
     <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+    <td style="background-color:#353988 ;">
+            <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-bottom:13px;"><span>Welcome !</span></p></tr>
+        </td>
+    </tr>
   <tr>
       <td align="center" style="padding: 0 20px">
-          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px">
-        <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;margin:0;margin-bottom:13px"><span>Welcome !</span></p>
+          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px; padding-top: 0px;">
         <div style="font-size:16px; line-height:24px;margin-bottom:16px">
           <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi ${name},</span>
           </p>
@@ -93,6 +97,7 @@ export async function sendTestInviteMail(email: string, link: string) {
   const from = 'careers@copods.co'
   const subject = 'Welcome to K - Quiz @ Copods'
   const text = 'K - Quiz @ Copods'
+  const logo = 'K - Quiz logo'
   const html = `<html>
   <head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -100,46 +105,49 @@ export async function sendTestInviteMail(email: string, link: string) {
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
   </head>
   <body>
-    <div style="min-height:200px;background: url('https://res.cloudinary.com/drpi0krkh/image/upload/v1656596230/User_Invitation_1_he7s2e.png');background-repeat:repeat-x;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;height:50px;text-align:center">
-      <img src="https://res.cloudinary.com/drpi0krkh/image/upload/c_scale,h_200/v1656065336/Logo_jtr0wl.png" alt=K-Quiz Logo height="48" />
-    </div>
-    <div>
-    <table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <div style="min-height:200px;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
+  <div style="background:#353988;padding:15px 0px;text-align:center">
+  <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30" />
+</div>
+<div  style="font-family:'Poppins', sans-serif;background-color:#F3F4F6">
+  <table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr>
-      <td align="center" style="padding: 0 20px">
-          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px">
-        <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;margin:0;margin-bottom:13px"><span>Welcome !</span></p>
-        <div style="font-size:16px; line-height:24px;margin-bottom:16px">
-          <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi,</span>
-          </p>
-          <br>
-          <p style="text-align:left;color:#4B5563;margin:0;">You have been invited by Copods Design Technology Solutions LLP for an online pre interview assessment test.
-          </p>
-          
-
-
-        </div>
-        <div style="text-align:center;padding:10px" >
-        <a href="${link}" style="text-decoration:none"><span style="padding: 10px 38px;background:#353988;color:#fff;font-family:'Poppins', sans-serif;">Click here to take the test</span></a>
-        </div>
-        <br>
-        
-          <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;">
-            If the above link does not work, copy and paste this URL into your browser:
-          </p>
-          <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;margin-top:10px">
-            <a href="${link}" style="color: #353988;">${link}</a>
-          </p>
-          <br>
-      </div>
+  <td style="background-color:#353988 ;">
+          <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-bottom:13px;"><span>Welcome !</span></p></tr>
       </td>
   </tr>
+      <tr>
+    <td align="center">
+        <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px; padding-top: 0px;">
+      <div style="font-size:16px; line-height:24px;margin-bottom:16px">
+        <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi,</span>
+        </p>
+        <br>
+        <p style="text-align:left;color:#4B5563;margin:0;">You have been invited by Copods Design Technology Solutions LLP for an online pre interview assessment test.
+        </p>
+        
+
+
+      </div>
+      <div style="text-align:center;padding:10px" >
+      <a href="${link}" style="text-decoration:none"><span style="padding: 10px 38px;background:#353988;color:#fff;font-family:'Poppins', sans-serif;">Click here to take the test</span></a>
+      </div>
+      <br>
+      
+        <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;">
+          If the above link does not work, copy and paste this URL into your browser:
+        </p>
+        <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;margin-top:10px">
+          <a href="${link}" style="color: #353988;">"${link}"</a>
+        </p>
+        <br>
+    </div>
+    </td>
+</tr>
 </table>
 
-      <div style="text-align:center;padding: 20px 20px 24px;font-family:'Poppins', sans-serif;">K - Quiz @ Copods</div>
-    </div>
-    </div>
+    <div style="text-align:center;padding: 20px 20px 24px;font-family:'Poppins', sans-serif;">K - Quiz @ Copods</div>
+  </div>
   </body>
 </html>
       `
@@ -172,6 +180,7 @@ export async function sendOTPMail(email: string, otp: number) {
   const from = 'careers@copods.co'
   const subject = 'OTP - Quiz @ Copods'
   const text = 'K - Quiz @ Copods'
+  const logo = 'K - Quiz logo'
   const html = `<html>
   <head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -179,16 +188,20 @@ export async function sendOTPMail(email: string, otp: number) {
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
   </head>
   <body>
-    <div style="min-height:200px;background: url('https://res.cloudinary.com/drpi0krkh/image/upload/v1656596230/User_Invitation_1_he7s2e.png');background-repeat:repeat-x;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;height:50px;text-align:center">
-      <img src="https://res.cloudinary.com/drpi0krkh/image/upload/c_scale,h_200/v1656065336/Logo_jtr0wl.png" alt="kQuiz" height="48" />
-    </div>
+    <div style="min-height:200px;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
+    <div style="background:#353988;padding:15px 0px;text-align:center">
+    <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30" />
+  </div>
     <div>
     <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>
+    <td style="background-color:#353988 ;">
+            <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-bottom:13px;"><span>Welcome !</span></p></tr>
+        </td>
+    </tr>
   <tr>
       <td align="center" style="padding: 0 20px">
-          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px">
-        <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;margin:0;margin-bottom:13px"><span>Welcome !</span></p>
+          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px; padding-top:0px;">
         <div style="font-size:16px; line-height:24px;margin-bottom:16px">
           <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi,</span></p><br>
 <p style="text-align:left;color:#4B5563;margin:0;"><span>This mail is sent for verification</span></p>
@@ -242,6 +255,7 @@ export async function sendMailToRecruiter(
   const from = 'careers@copods.co'
   const subject = `${candidateName} has finished the assessment of K - Quiz @ Copods`
   const text = 'K - Quiz @ Copods'
+  const logo = 'K - Quiz logo'
   const html = `<html>
   <head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -250,9 +264,9 @@ export async function sendMailToRecruiter(
   </head>
   <body>
     <div style="min-height:200px;background: url('https://res.cloudinary.com/drpi0krkh/image/upload/v1656596230/User_Invitation_1_he7s2e.png');background-repeat:repeat-x;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;height:50px;text-align:center">
-      <img src="https://res.cloudinary.com/drpi0krkh/image/upload/c_scale,h_200/v1656065336/Logo_jtr0wl.png" alt="kQuiz" height="48" />
-    </div>
+    <div style="background:#353988;padding:15px 0px;text-align:center">
+    <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30"/>
+  </div>
     <div>
     <table width="100%" border="0" cellspacing="0" cellpadding="0">
   <tr>
@@ -317,6 +331,7 @@ export async function sendPassword(email: string, password: string) {
   const from = 'careers@copods.co'
   const subject = 'RESET PASSWORD - Quiz @ Copods'
   const text = 'K - Quiz @ Copods'
+  const logo = 'K - Quiz logo'
   const html = `<html>
   <head>
   <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -324,16 +339,20 @@ export async function sendPassword(email: string, password: string) {
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
   </head>
   <body>
-    <div style="min-height:200px;background: url('https://res.cloudinary.com/drpi0krkh/image/upload/v1656596230/User_Invitation_1_he7s2e.png');background-repeat:repeat-x;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;height:50px;text-align:center">
-      <img src="https://res.cloudinary.com/drpi0krkh/image/upload/c_scale,h_200/v1656065336/Logo_jtr0wl.png" alt="kQuiz" height="48" />
-    </div>
+    <div style="min-height:200px;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
+    <div style="background:#353988;padding:15px 0px;text-align:center">
+    <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30" />
+  </div>
     <div>
     <table width="100%" border="0" cellspacing="0" cellpadding="0">
+    <tr>s
+    <td style="background-color:#353988 ;">
+            <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-bottom:13px;"><span>Welcome !</span></p></tr>
+        </td>
+    </tr>
   <tr>
       <td align="center" style="padding: 0 20px">
-          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px">
-        <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;margin:0;margin-bottom:13px"><span>Welcome !</span></p>
+          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-top:0px;">
         <div style="font-size:16px; line-height:24px;margin-bottom:16px">
           <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi,</span></p><br>
 <p style="text-align:left;color:#4B5563;margin:0;"><span>Your new password has been generated</span></p>
