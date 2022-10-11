28fb7672231ee978b13949572d7db9ab9ed049d2
arpita-20 <arpita@copods.co>
2022-10-11 11:18:47 +0530

    fix(): default routing changed to members

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -7,14 +7,14 @@ import { routes } from '~/constants/route.constants'
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await getUserId(request)
-  if (userId) return redirect(routes.dashboard)
+  if (userId) return redirect(routes.members)
   return redirect(routes.signIn)
 }
 
 export default function Index() {
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
-      <Link tabIndex={0} to={routes.dashboard}>
+      <Link tabIndex={0} to={routes.members}>
         <Logo width="192" height="192" />
       </Link>
     </main>
