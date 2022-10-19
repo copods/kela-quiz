007231ffa1409d382dcce2470dd26e131a6cfe69
anuragP04 <106807559+anuragP04@users.noreply.github.com>
2022-10-19 20:48:33 +0530

    Wrong answerfix (#266)
    * added results: wrong and incorrect questions
    
    * format  fix

--- app/components/results/SectionCardForResultDetail.tsx
+++ app/components/results/SectionCardForResultDetail.tsx
@@ -5,11 +5,15 @@ const SectionCardForResultDetail = ({
   totalQuestions,
   correctQuestions,
   skippedQuestions,
+  incorrectQuestions,
+  unansweredQuestions,
 }: {
   name: string
   totalQuestions: number
   correctQuestions: number
   skippedQuestions: number
+  incorrectQuestions: number
+  unansweredQuestions: number
 }) => {
   const { t } = useTranslation()
   return (
@@ -21,9 +25,21 @@ const SectionCardForResultDetail = ({
           <span className="font-medium text-green-600">{correctQuestions}</span>
         </div>
         <div className="flex gap-2 text-sm">
+          <span className="text-gray-500">
+            {t('resultConstants.incorrect')}:
+          </span>
+          <span className="font-medium text-red-600">{incorrectQuestions}</span>
+        </div>
+        <div className="flex gap-2 text-sm">
           <span className="text-gray-500">{t('resultConstants.skipped')}:</span>
+          <span className="font-medium text-zinc-600">{skippedQuestions}</span>
+        </div>
+        <div className="flex gap-2 text-sm">
+          <span className="text-gray-500">
+            {t('resultConstants.unanswered')}:
+          </span>
           <span className="font-medium text-yellow-600">
-            {skippedQuestions}
+            {unansweredQuestions}
           </span>
         </div>
         <div className="flex gap-2 text-sm">
