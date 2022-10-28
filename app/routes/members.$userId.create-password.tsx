3a5891ef86af7d9a2346ea57597cf1bbbf0484c7
yash thakur <yash.thakur@copods.co>
2022-10-28 13:10:28 +0530

    add-link-check

--- app/routes/members.$userId.create-password.tsx
+++ app/routes/members.$userId.create-password.tsx
@@ -5,22 +5,35 @@ import { redirect } from '@remix-run/node'
 import { useActionData } from '@remix-run/react'
 import type { LoaderFunction, ActionFunction } from '@remix-run/server-runtime'
 import invariant from 'tiny-invariant'
-import { createPasswordOfUser } from '~/models/user.server'
+import { createPasswordOfUser, getAllUsers } from '~/models/user.server'
 import { routes } from '~/constants/route.constants'
 
 export const loader: LoaderFunction = async ({ request, params }) => {
+  const getUsers = await getAllUsers()
+  const userId = getUsers.filter((item) => {
+    return item?.id === params.userId
+  })
+  if (userId[0]?.id !== params.userId) {
+    throw new Response('Not Found', {
+      status: 404,
+    })
+  }
   invariant(params.userId, 'testId not found')
+
   return null
 }
 export const action: ActionFunction = async ({ request, params }) => {
   const formData = await request.formData()
   const userId = params.userId
+
   const enteredPassword = formData.get('enterPassword')
   const reEnteredPassword = formData.get('reEnterPassword')
+
   if (enteredPassword === reEnteredPassword) {
     await createPasswordOfUser(userId as string, enteredPassword as string)
     return redirect(routes.signIn)
   }
+
   if (enteredPassword !== reEnteredPassword) {
     return 'not equal'
   }
