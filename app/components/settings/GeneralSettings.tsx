5780f7ff6d641734e2b5fbcd565e285ec399293b
akshay-copods <akshay.kamble@copods.co>
2022-11-15 10:41:07 +0530

    style: change alignment

--- app/components/settings/GeneralSettings.tsx
+++ app/components/settings/GeneralSettings.tsx
@@ -11,22 +11,22 @@ const GeneralSettings = () => {
       <div className="flex flex-col gap-8 rounded-lg border border-solid border-gray-300 bg-white p-4">
         <h3 className="text-lg font-semibold"> {t('settings.basicInfo')}</h3>
         <div className="flex flex-col gap-5">
-          <div className="flex justify-between gap-4">
-            <span className="text-base font-medium text-gray-700">
+          <div className="flex flex-col justify-between gap-4">
+            <span className="text-base font-bold text-gray-700">
               {t('settings.version')}
             </span>
             <span className="ext-base font-medium text-gray-600">
               {version}
             </span>
           </div>
-          <div className="flex justify-between gap-4">
-            <span className="text-base font-medium text-gray-700">
+          <div className="flex flex-col justify-between gap-4">
+            <span className="text-base font-bold text-gray-700">
               {t('settings.password')}
             </span>
             <button
               tabIndex={0}
               onClick={() => setOpenResetPassModel(!openResetPassModel)}
-              className="resetPassOpenPopUpLink cursor-pointer text-base font-medium text-primary"
+              className="resetPassOpenPopUpLink cursor-pointer self-start text-base font-medium text-primary"
             >
               {t('settings.clickToChange')}
             </button>
