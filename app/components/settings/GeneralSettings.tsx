34be08794ab9a77bd58acc12cbc5c2f4ce085b93
akshay-copods <117165556+akshay-copods@users.noreply.github.com>
2022-11-16 10:54:29 +0530

    Gap between label and value should be reduced in settings #306 (#341)
    * style: change alignment

--- app/components/settings/GeneralSettings.tsx
+++ app/components/settings/GeneralSettings.tsx
@@ -8,25 +8,25 @@ const GeneralSettings = () => {
   const { t } = useTranslation()
   return (
     <>
-      <div className="flex flex-col gap-8 rounded-lg border border-solid border-gray-300 bg-white p-4">
+      <div className="flex flex-col gap-4 rounded-lg border border-solid border-gray-300 bg-white p-4">
         <h3 className="text-lg font-semibold"> {t('settings.basicInfo')}</h3>
         <div className="flex flex-col gap-5">
-          <div className="flex justify-between gap-4">
-            <span className="text-base font-medium text-gray-700">
+          <div className="flex flex-col gap-2">
+            <span className="text-base font-bold text-gray-700">
               {t('settings.version')}
             </span>
             <span className="ext-base font-medium text-gray-600">
               {version}
             </span>
           </div>
-          <div className="flex justify-between gap-4">
-            <span className="text-base font-medium text-gray-700">
+          <div className="flex flex-col gap-2">
+            <span className="text-base font-bold text-gray-700">
               {t('settings.password')}
             </span>
             <button
               tabIndex={0}
               onClick={() => setOpenResetPassModel(!openResetPassModel)}
-              className="resetPassOpenPopUpLink cursor-pointer text-base font-medium text-primary"
+              className="resetPassOpenPopUpLink cursor-pointer self-start text-base font-medium text-primary"
             >
               {t('settings.clickToChange')}
             </button>
