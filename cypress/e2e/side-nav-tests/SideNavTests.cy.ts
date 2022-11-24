86ea03c85e3a2754b0bcb99698841056d4cc73cc
akshay-copods <akshay.kamble@copods.co>
2022-11-24 16:33:04 +0530

    refactor: add tests for sideNav footer

--- cypress/e2e/side-nav-tests/SideNavTests.cy.ts
+++ cypress/e2e/side-nav-tests/SideNavTests.cy.ts
@@ -5,11 +5,14 @@ import {
   testsConstants,
 } from '~/constants/common.constants'
 import { routes } from '~/constants/route.constants'
-const sideNavTitle = {
+const sideNavGroupTitles = {
   results: 'Results',
   assessments: 'Assessments',
   general: 'General',
 }
+const userName = 'Copods Careers'
+const userEmail = 'copods.demo.sendgrid@gmail.com'
+const userAvatarText = 'CC'
 describe('Test for Logout, SideNav', () => {
   beforeEach('sign-in', () => {
     cy.login()
@@ -49,35 +52,35 @@ describe('Test for Logout, SideNav', () => {
       })
     })
   })
-  it('Checks text SideNavTitle- Results', () => {
-    cy.get('#nav-guide-results').should('have.text', sideNavTitle.results)
+  it('Checks text SideNav group title- Results', () => {
+    cy.get('#nav-guide-results').should('have.text', sideNavGroupTitles.results)
   })
-  it('Checks font weight of SideNavTitle- Results', () => {
+  it('Checks font weight of SideNav group title- Results', () => {
     cy.get('#nav-guide-results').should('have.css', 'font-weight', '600')
   })
-  it('Checks font-size of SideNavTitle- Results ', () => {
+  it('Checks font-size of SideNav group title- Results ', () => {
     cy.get('#nav-guide-results').should('have.css', 'font-size', '12px')
   })
-  it('Checks color of SideNavTitle- Results ', () => {
+  it('Checks color of SideNav group title- Results ', () => {
     cy.get('#nav-guide-results').should(
       'have.css',
       'color',
       'rgb(156, 163, 175)'
     )
   })
-  it('Checks text of SideNavTitle- Assessments ', () => {
+  it('Checks text of SideNav group title- Assessments ', () => {
     cy.get('#nav-guide-assessments').should(
       'have.text',
-      sideNavTitle.assessments
+      sideNavGroupTitles.assessments
     )
   })
-  it('Checks font weight of SideNavTitle- Assessments ', () => {
+  it('Checks font weight of SideNav group title- Assessments ', () => {
     cy.get('#nav-guide-assessments').should('have.css', 'font-weight', '600')
   })
-  it('Checks font-size of SideNavTitle- Assessments ', () => {
+  it('Checks font-size of SideNav group title- Assessments ', () => {
     cy.get('#nav-guide-assessments').should('have.css', 'font-size', '12px')
   })
-  it('Checks color of SideNavTitle- Assessments ', () => {
+  it('Checks color of SideNav group title- Assessments ', () => {
     cy.get('#nav-guide-assessments').should(
       'have.css',
       'color',
@@ -85,22 +88,66 @@ describe('Test for Logout, SideNav', () => {
     )
   })
 
-  it('Checks text SideNavTitle- General', () => {
-    cy.get('#nav-guide-general').should('have.text', sideNavTitle.general)
+  it('Checks text SideNav group title- General', () => {
+    cy.get('#nav-guide-general').should('have.text', sideNavGroupTitles.general)
   })
-  it('Checks font weight of SideNavTitle- General ', () => {
+  it('Checks font weight of SideNav group title- General ', () => {
     cy.get('#nav-guide-general').should('have.css', 'font-weight', '600')
   })
-  it('Checks font size of SideNavTitle- General ', () => {
+  it('Checks font size of SideNav group title- General ', () => {
     cy.get('#nav-guide-general').should('have.css', 'font-size', '12px')
   })
-  it('Checks color of SideNavTitle- General ', () => {
+  it('Checks color of SideNav group title- General ', () => {
     cy.get('#nav-guide-general').should(
       'have.css',
       'color',
       'rgb(156, 163, 175)'
     )
   })
+  it('Checks for SideNav username text', () => {
+    cy.get('#sidenav-username').should('have.text', userName)
+  })
+  it('Checks for SideNav username color', () => {
+    cy.get('#sidenav-username').should('have.css', 'color', 'rgb(17, 24, 39)')
+  })
+  it('Checks for SideNav username font weight', () => {
+    cy.get('#sidenav-username').should('have.css', 'font-weight', '600')
+  })
+  it('Checks for SideNav username font size', () => {
+    cy.get('#sidenav-username').should('have.css', 'font-size', '12px')
+  })
+  it('Checks for sideNav user email text', () => {
+    cy.get('#sidenav-user-email').should('have.text', userEmail)
+  })
+
+  it('Checks for sideNav user email color', () => {
+    cy.get('#sidenav-user-email').should(
+      'have.css',
+      'color',
+      'rgb(107, 114, 128)'
+    )
+  })
+  it('Checks for sideNav user email font size', () => {
+    cy.get('#sidenav-user-email').should('have.css', 'font-size', '12px')
+  })
+  // user avatar
+  it('Checks for sideNav user avatar text', () => {
+    cy.get('#sidenav-user-avatar').should('have.text', userAvatarText)
+  })
+  it('Checks for sideNav user avatar font size', () => {
+    cy.get('#sidenav-user-avatar').should('have.css', 'font-size', '18px')
+  })
+  it('Checks for sideNav user avatar font weight', () => {
+    cy.get('#sidenav-user-avatar').should('have.css', 'font-weight', '500')
+  })
+  it('Checks for sideNav user avatar color', () => {
+    cy.get('#sidenav-user-avatar').should(
+      'have.css',
+      'color',
+      'rgb(255, 255, 255)'
+    )
+  })
+  // logout CTA button
   it('Checks for logout CTA button background color', () => {
     cy.get('#logout-button').should(
       'have.css',
