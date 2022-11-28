4b517a833d60a16ecd3c9d885d6b9dd820fd15a2
akshay-copods <117165556+akshay-copods@users.noreply.github.com>
2022-11-28 17:36:40 +0530

    421 - On login, show common error "Incorrect email or passsword" (#433)
    * refactor: update error text for authentication

--- app/routes/sign-in.tsx
+++ app/routes/sign-in.tsx
@@ -57,14 +57,14 @@ export const action: ActionFunction = async ({ request }) => {
   const user = await loginVerificationResponse(email, password)
   if (!user) {
     return json<ActionData>(
-      { errors: { email: 'statusCheck.emailIsInvalid' } },
+      { errors: { password: 'statusCheck.incorrectEmailOrPassword' } },
       { status: 400 }
     )
   }
 
   if (user instanceof Error) {
     return json<ActionData>(
-      { errors: { password: 'statusCheck.passIsInvalid' } },
+      { errors: { password: 'statusCheck.incorrectEmailOrPassword' } },
       { status: 400 }
     )
   }
