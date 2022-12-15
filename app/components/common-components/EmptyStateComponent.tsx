3991f1e1ba6f53b97d09d89a49bd000d283cc259
akshay-copods <akshay.kamble@copods.co>
2022-12-15 10:16:53 +0530

    refactor: update svg and spacing

--- app/components/common-components/EmptyStateComponent.tsx
+++ app/components/common-components/EmptyStateComponent.tsx
@@ -3,7 +3,7 @@ import noData from '../../../public/assets/noData.svg'
 
 const EmptyStateComponent = () => {
   return (
-    <div className="flex h-full flex-col items-center justify-center">
+    <div className="flex h-full flex-col items-center justify-center gap-16">
       <img src={noData} alt={t('candidateExamConstants.alreadySubmitted')} />
       <span className="text-2xl font-bold">
         {t('sectionsConstants.noDataToShow')}
