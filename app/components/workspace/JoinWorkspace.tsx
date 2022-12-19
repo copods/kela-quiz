9003d975dae94e79620df087a2b7c0adae987875
Copods <copods@Yash-Thakur-Copods.local>
2022-12-19 19:33:12 +0530

    url-updated

--- app/components/workspace/JoinWorkspace.tsx
+++ app/components/workspace/JoinWorkspace.tsx
@@ -3,13 +3,12 @@ import { useTranslation } from 'react-i18next'
 import Button from '../common-components/Button'
 import logo from '../../../public/assets/member-invitation.svg'
 import { useLoaderData } from '@remix-run/react'
+import { Form } from '@remix-run/react'
 
 const JoinWorkspace = () => {
   const { t } = useTranslation()
   let navigate = useNavigate()
   const workspcaceInvitationData = useLoaderData()
-  const workspaceInvitation = workspcaceInvitationData.invitedMember
-
   return (
     <div className="flex h-full flex-col items-center justify-center gap-6 rounded-lg">
       <div className="rounded-lg border-solid border-gray-50 bg-white px-14 py-10 shadow-sm">
@@ -30,7 +29,7 @@ const JoinWorkspace = () => {
                   {t('members.alreadyJoinedWorkspace')}
                 </span>
               ) : workspcaceInvitationData?.loginWithWrongId ? (
-                <span className="text-center text-primary">
+                <span className="text-center text-base text-gray-500">
                   {t('members.loggedinFromAnotherAccount')}
                 </span>
               ) : null}
@@ -39,9 +38,9 @@ const JoinWorkspace = () => {
               {workspcaceInvitationData === 'joined' ? (
                 <Button
                   tabIndex={0}
-                  title="Go to Dashboard"
+                  title={t('404.gotoDashboard')}
                   id="go-to-dashboard"
-                  buttonText="Go to Dashboard"
+                  buttonText={t('404.gotoDashboard')}
                   name="go-to-workspace"
                   varient="primary-outlined"
                   value={'reject'}
@@ -52,10 +51,39 @@ const JoinWorkspace = () => {
                     )
                   }
                 />
-              ) : workspaceInvitation?.joined === false ? (
-                ''
               ) : workspcaceInvitationData?.loginWithWrongId ? (
-                ''
+                <div className="flex gap-8">
+                  <Button
+                    tabIndex={0}
+                    title={t('404.gotoDashboard')}
+                    id="go-to-dashboard"
+                    buttonText={t('404.gotoDashboard')}
+                    name="go-to-workspace"
+                    varient="primary-outlined"
+                    value={'reject'}
+                    className="py-3 px-9"
+                    onClick={() =>
+                      navigate(
+                        `/sign-in??cameFrom=join&id=${workspcaceInvitationData.inviteId}`
+                      )
+                    }
+                  />
+                  <Form
+                    action={`/logout??cameFrom=join&id=${workspcaceInvitationData.invitedMember.id}`}
+                    method="post"
+                  >
+                    <Button
+                      tabIndex={0}
+                      title={t('commonConstants.switchAccount')}
+                      id={t('commonConstants.switchAccount')}
+                      buttonText={t('commonConstants.switchAccount')}
+                      name="logout"
+                      varient="primary-solid"
+                      value={t('commonConstants.switchAccount')}
+                      className="py-3 px-9"
+                    />
+                  </Form>
+                </div>
               ) : null}
             </div>
           </div>
