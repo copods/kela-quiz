ba806c760310c8243c7bdd8fbf52d7dccf8a5a24
Copods <copods@Yash-Thakur-Copods.local>
2023-01-06 12:15:02 +0530

    fix:testActionMenu

--- app/components/TestListActionMenu.tsx
+++ app/components/TestListActionMenu.tsx
@@ -48,7 +48,7 @@ const TestListMenuItem = ({
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items className="absolute right-0 z-40 origin-bottom-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+          <Menu.Items className="absolute right-0 z-40 mr-20 origin-bottom-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
             <div className="px-1 py-1">
               <Menu.Item>
                 {({ active }) => (
