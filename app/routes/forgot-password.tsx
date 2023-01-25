ce011587da92745b481c3fd012ce8cf243fdf029
Anurag <anurag-copods@sprih.com>
2023-01-25 14:42:35 +0530

    fix(): formatting change

--- app/routes/forgot-password.tsx
+++ app/routes/forgot-password.tsx
@@ -1,68 +1,20 @@
 import UserForgetPassword from '~/components/login/UserForgetPassword'
 import type { ActionFunction } from '@remix-run/node'
-import { sendResetPassword, getUserByEmail } from '~/models/user.server'
+import { sendResetPassword } from '~/models/user.server'
 import { useEffect, useState } from 'react'
 import { useActionData, useNavigate } from '@remix-run/react'
 import { toast } from 'react-toastify'
 import { routes } from '~/constants/route.constants'
 import { useTranslation } from 'react-i18next'
-import { json } from '@remix-run/node'
-
-export type ActionData = {
-  errors?: {
-    title: string
-    status: number
-    emailNotFound?: boolean
-  }
-  resp?: {
-    title: string
-    status: number
-  }
-}
-
-const errResponse = (emailNotFound: boolean) => {
-  let title = 'statusCheck.sendGridError'
-  let errRes = json<ActionData>(
-    {
-      errors: {
-        title,
-        status: 400,
-        emailNotFound: emailNotFound,
-      },
-    },
-    { status: 400 }
-  )
-
-  return errRes
-}
 
 export const action: ActionFunction = async ({ request, params }) => {
   const formData = await request.formData()
   const email = formData.get('email') as string
   let sendResetPasswordResponse = null
-  try {
-    const userFound = await getUserByEmail(email)
-
-    if (!userFound) {
-      return errResponse(true)
-    }
-
+  if (email) {
     sendResetPasswordResponse = await sendResetPassword(email as string)
-    if (email && sendResetPasswordResponse) {
-      return json<ActionData>(
-        {
-          resp: {
-            title: 'statusCheck.resendPasswordSuccess',
-            status: 200,
-          },
-        },
-        { status: 200 }
-      )
-    }
-    return sendResetPasswordResponse
-  } catch (err) {
-    return errResponse(false)
   }
+  return sendResetPasswordResponse
 }
 
 const ForgetPassword = () => {
@@ -71,14 +23,12 @@ const ForgetPassword = () => {
   const [checkErrorStatus, setCheckErrorStatus] = useState(false)
   const action = useActionData()
   useEffect(() => {
-    if (action?.resp?.status === 200) {
-      toast.success(t(action?.resp?.title))
+    if (action?.value === null) {
+      setCheckErrorStatus(true)
+    }
+    if (action === 'Done') {
+      toast.success(t('statusCheck.resendPasswordSuccess'))
       navigate(routes.signIn)
-    } else if (action?.errors?.status === 400) {
-      action?.errors?.emailNotFound && setCheckErrorStatus(true)
-      toast.error(t(action?.errors?.title), {
-        toastId: action?.errors?.title,
-      })
     }
   }, [action, navigate, t, action?.time])
   return (
