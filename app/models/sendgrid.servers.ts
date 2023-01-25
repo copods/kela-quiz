a459b54a9771dac7b502987a042b8e332662907a
anuragP04 <106807559+anuragP04@users.noreply.github.com>
2023-01-25 14:56:56 +0530

    Sendgrid update (#560)
    fix(): formatting change

--- app/models/sendgrid.servers.ts
+++ app/models/sendgrid.servers.ts
@@ -1,6 +1,45 @@
 import sendgrid from '@sendgrid/mail'
 import { env } from 'process'
 
+import {
+  WELCOME_TEMPLATE,
+  TEST_INVITE_TEMPLATE,
+  OTP_TEMPLATE,
+  RECRUITER_TEMPLATE,
+  NEW_OTP_TEMPLATE,
+  MEMBER_INVITE_TEMPLATE,
+  sendGridFailed,
+} from '~/constants/email.constants'
+
+const callSengridAPI = ({
+  to,
+  from, // Use the email address or domain you verified above
+  subject,
+  text,
+  html,
+}: any) => {
+  sendgrid.setApiKey(env.SENDGRID_API_KEY as string)
+
+  const msg = {
+    to,
+    from, // Use the email address or domain you verified above
+    subject,
+    text,
+    html,
+  }
+  return sendgrid
+    .send(msg)
+    .then((res: any) => {
+      if (res[0].statusCode === 202) return 'Done'
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.error('Sendgrid Error: ', error.response.body)
+      }
+      throw new Error(sendGridFailed)
+    })
+}
+
 export async function sendMail(
   passwordGenerationLink: string,
   email: string,
@@ -12,80 +51,24 @@ export async function sendMail(
   const subject = 'Welcome to K - Quiz @ Copods'
   const text = 'K - Quiz @ Copods'
   const logo = 'K - Quiz logo'
-  const html = `<html>
-  <head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <div style="min-height:200px;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;padding:15px 0px;text-align:center">
-    <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30" />
-  </div>
-    <div>
-    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-    <td style="background-color:#353988;padding-right:20px;padding-left:20px">
-            <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-bottom:13px;"><span>Welcome!</span></p></tr>
-        </td>
-    </tr>
-  <tr>
-      <td align="center" style="padding: 0 20px">
-          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px; padding-top: 0px;">
-        <div style="font-size:16px; line-height:24px;margin-bottom:16px">
-          <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi ${name},</span>
-          </p>
-          <br>
-          <p style="text-align:left;color:#4B5563;margin:0;">You have been added as ${role} on K - Quiz.
-          </p>
-          <br>
+  const html = WELCOME_TEMPLATE({
+    passwordGenerationLink,
+    logo,
+    name,
+    role,
+  })
 
-          <p style="text-align:left;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;">
-          Please use below link to create your first password.
-          </p>
-          <br>
-        </div>        
-        <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;margin-top:10px">     
-          <a href="${passwordGenerationLink}" style="color: #353988;">${passwordGenerationLink}</a>
-        </p>
-        <br>
-         <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;">
-             Use above link to generate your password
-          </p>
-      </div>
-      </td>
-  </tr>
-</table>
-      
-      <div style="text-align:center;padding: 20px 20px 24px;font-family:'Poppins', sans-serif;">K - Quiz @ Copods</div>
-    </div>
-    </div>
-  </body>
-</html>
-    `
-
-  sendgrid.setApiKey(env.SENDGRID_API_KEY as string)
-
-  const msg = {
-    to,
-    from, // Use the email address or domain you verified above
-    subject,
-    text,
-    html,
+  try {
+    return await callSengridAPI({
+      to,
+      from, // Use the email address or domain you verified above
+      subject,
+      text,
+      html,
+    })
+  } catch (err) {
+    throw new Error(sendGridFailed)
   }
-  await sendgrid.send(msg).then(
-    () => {
-      return 'ok'
-    },
-    (error) => {
-      if (error.response) {
-        console.error('Sendgrid Error: ', error.response.body)
-      }
-    }
-  )
-
-  return 'Done'
 }
 
 export async function sendTestInviteMail(email: string, link: string) {
@@ -94,81 +77,19 @@ export async function sendTestInviteMail(email: string, link: string) {
   const subject = 'Welcome to K - Quiz @ Copods'
   const text = 'K - Quiz @ Copods'
   const logo = 'K - Quiz logo'
-  const html = `<html>
-  <head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
-  </head>
-  <body>
-  <div style="min-height:200px;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-  <div style="background:#353988;padding:15px 0px;text-align:center">
-  <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30" />
-</div>
-<div  style="font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-  <td style="background-color:#353988;padding-right:20px;padding-left:20px">
-          <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-bottom:13px;"><span>Welcome!</span></p></tr>
-      </td>
-  </tr>
-      <tr>
-    <td align="center">
-        <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px; padding-top: 0px;">
-      <div style="font-size:16px; line-height:24px;margin-bottom:16px">
-        <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi,</span>
-        </p>
-        <br>
-        <p style="text-align:left;color:#4B5563;margin:0;">You have been invited by Copods for an online pre interview assessment test.
-        </p>
-        
+  const html = TEST_INVITE_TEMPLATE({ logo, link })
 
-
-      </div>
-      <div style="text-align:center;padding:10px" >
-      <a href="${link}" style="text-decoration:none"><span style="padding: 10px 38px;background:#353988;color:#fff;font-family:'Poppins', sans-serif;">Click here to take the test</span></a>
-      </div>
-      <br>
-      
-        <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;">
-          If the above link does not work, copy and paste this URL into your browser:
-        </p>
-        <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;margin-top:10px">
-          <a href="${link}" style="color: #353988;">"${link}"</a>
-        </p>
-        <br>
-    </div>
-    </td>
-</tr>
-</table>
-
-    <div style="text-align:center;padding: 20px 20px 24px;font-family:'Poppins', sans-serif;">K - Quiz @ Copods</div>
-  </div>
-  </body>
-</html>
-      `
-
-  sendgrid.setApiKey(env.SENDGRID_API_KEY as string)
-
-  const msg = {
-    to,
-    from, // Use the email address or domain you verified above
-    subject,
-    text,
-    html,
+  try {
+    return await callSengridAPI({
+      to,
+      from, // Use the email address or domain you verified above
+      subject,
+      text,
+      html,
+    })
+  } catch (err) {
+    throw new Error(sendGridFailed)
   }
-  await sendgrid.send(msg).then(
-    () => {
-      return 'ok'
-    },
-    (error) => {
-      if (error.response) {
-        console.error('Sendgrid Error: ', error.response.body)
-      }
-    }
-  )
-
-  return 'Done'
 }
 
 export async function sendOTPMail(email: string, otp: number) {
@@ -177,71 +98,21 @@ export async function sendOTPMail(email: string, otp: number) {
   const subject = 'OTP - Quiz @ Copods'
   const text = 'K - Quiz @ Copods'
   const logo = 'K - Quiz logo'
-  const html = `<html>
-  <head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <div style="min-height:200px;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;padding:15px 0px;text-align:center">
-    <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30" />
-  </div>
-    <div>
-    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-    <td style="background-color:#353988;padding-right:20px;padding-left:20px">
-            <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-bottom:13px;"><span>Welcome!</span></p></tr>
-        </td>
-    </tr>
-  <tr>
-      <td align="center" style="padding: 0 20px">
-          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px; padding-top:0px;">
-        <div style="font-size:16px; line-height:24px;margin-bottom:16px">
-          <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi,</span></p><br>
-<p style="text-align:left;color:#4B5563;margin:0;"><span>This mail is sent for verification</span></p>
-          </p>
-          <br>
-          <p style="text-align:left;color:#4B5563;margin:0;">Your OTP is <b>${otp}</b>
-          </p>
-          <br>
-<br>
-<p style="text-align:left;color:#4B5563;margin:0;"><span>Please enter this OTP to start the K-Quiz test</span></p>
-      </div>
-      </td>
-  </tr>
-</table>
+  const html = OTP_TEMPLATE({ logo, otp })
 
-      <div style="text-align:center;padding: 20px 20px 24px;font-family:'Poppins', sans-serif;">K - Quiz @ Copods</div>
-    </div>
-    </div>
-  </body>
-  </html>
-</html>
-      `
-
-  sendgrid.setApiKey(env.SENDGRID_API_KEY as string)
-
-  const msg = {
-    to,
-    from, // Use the email address or domain you verified above
-    subject,
-    text,
-    html,
+  try {
+    return await callSengridAPI({
+      to,
+      from, // Use the email address or domain you verified above
+      subject,
+      text,
+      html,
+    })
+  } catch (err) {
+    throw new Error(sendGridFailed)
   }
-  await sendgrid.send(msg).then(
-    () => {
-      return 'ok'
-    },
-    (error) => {
-      if (error.response) {
-        console.error('Sendgrid Error: ', error.response.body)
-      }
-    }
-  )
-  return 'Done'
 }
+
 export async function sendMailToRecruiter(
   recruiterEmail?: string,
   testName?: string,
@@ -252,146 +123,47 @@ export async function sendMailToRecruiter(
   const subject = `${candidateName} has finished the assessment of K - Quiz @ Copods`
   const text = 'K - Quiz @ Copods'
   const logo = 'K - Quiz logo'
-  const html = `<html>
-  <head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <div style="min-height:200px;background: url('https://res.cloudinary.com/drpi0krkh/image/upload/v1656596230/User_Invitation_1_he7s2e.png');background-repeat:repeat-x;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;padding:15px 0px;text-align:center">
-    <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30"/>
-  </div>
-    <div>
-    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-  <tr>
-      <td align="center" style="padding: 0 20px">
-          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px">
-        <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;margin:0;margin-bottom:13px"><span>Welcome!</span></p>
-        <div style="font-size:16px; line-height:24px;margin-bottom:16px">
-          <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi,</span>
-          </p>
-          <br>
-          <p style="text-align:left;color:#4B5563;margin:0;">Candidate <b>${candidateName}</b> has finished the test <b>${testName}</b>.
-          </p>
-          
+  const html = RECRUITER_TEMPLATE({
+    logo,
+    candidateName,
+    testName,
+    PUBLIC_URL: env.PUBLIC_URL,
+  })
 
-
-        </div>
-       
-        <br>
-        
-          <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;">You can check the result on <a href="${env.PUBLIC_URL}" style="color: #353988;">admin panel</a>
-          </p>
-          <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;margin-top:10px">
-           
-          </p>
-          <br>
-      </div>
-      </td>
-  </tr>
-</table>
-
-      <div style="text-align:center;padding: 20px 20px 24px;font-family:'Poppins', sans-serif;">K - Quiz @ Copods</div>
-    </div>
-    </div>
-  </body>
-</html>
-      `
-
-  sendgrid.setApiKey(env.SENDGRID_API_KEY as string)
-
-  const msg = {
-    to,
-    from, // Use the email address or domain you verified above
-    subject,
-    text,
-    html,
+  try {
+    return await callSengridAPI({
+      to,
+      from, // Use the email address or domain you verified above
+      subject,
+      text,
+      html,
+    })
+  } catch (err) {
+    throw new Error(sendGridFailed)
   }
-  await sendgrid.send(msg).then(
-    () => {
-      return 'ok'
-    },
-    (error) => {
-      if (error.response) {
-        console.error('Sendgrid Error: ', error.response.body)
-      }
-    }
-  )
-
-  return 'Done'
 }
+
 export async function sendNewPassword(email: string, password: string) {
   const to = email
   const from = 'careers@copods.co'
   const subject = 'RESET PASSWORD - Quiz @ Copods'
   const text = 'K - Quiz @ Copods'
   const logo = 'K - Quiz logo'
-  const html = `<html>
-  <head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <div style="min-height:200px;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;padding:15px 0px;text-align:center">
-    <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30" />
-  </div>
-    <div>
-    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-    <td style="background-color:#353988;padding-right:20px;padding-left:20px">
-            <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-bottom:13px;"><span>Welcome!</span></p></tr>
-        </td>
-    </tr>
-  <tr>
-      <td align="center" style="padding: 0 20px">
-          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-top:0px;">
-        <div style="font-size:16px; line-height:24px;margin-bottom:16px">
-          <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi,</span></p><br>
-<p style="text-align:left;color:#4B5563;margin:0;"><span>Your new password has been generated</span></p>
-          </p>
-          <br>
-          <p style="text-align:left;color:#4B5563;margin:0;">Your new password is <b>${password}</b>
-          </p>
-          <br>
-<br>
-<p style="text-align:left;color:#4B5563;margin:0;"><span>Please enter this password to login</span></p>
-      </div>
-      </td>
-  </tr>
-</table>
-      <div style="text-align:center;padding: 20px 20px 24px;font-family:'Poppins', sans-serif;">K - Quiz @ Copods</div>
-    </div>
-    </div>
-  </body>
-  </html>
-</html>
-      `
+  const html = NEW_OTP_TEMPLATE({ logo, password })
 
-  sendgrid.setApiKey(env.SENDGRID_API_KEY as string)
-
-  const msg = {
-    to,
-    from, // Use the email address or domain you verified above
-    subject,
-    text,
-    html,
+  try {
+    return await callSengridAPI({
+      to,
+      from, // Use the email address or domain you verified above
+      subject,
+      text,
+      html,
+    })
+  } catch (err) {
+    throw new Error(sendGridFailed)
   }
-  await sendgrid.send(msg).then(
-    () => {
-      return 'ok'
-    },
-    (error) => {
-      if (error.response) {
-        console.error('Sendgrid Error: ', error.response.body)
-      }
-    }
-  )
-  return 'Done'
 }
+
 export async function sendMemberInvite(
   email: string,
   name: string,
@@ -402,77 +174,17 @@ export async function sendMemberInvite(
   const subject = 'Welcome to K - Quiz @ Copods'
   const text = 'K - Quiz @ Copods'
   const logo = 'K - Quiz logo'
-  const html = `<html>
-  <head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
-  </head>
-  <body>
-    <div style="min-height:200px;font-family:'Poppins', sans-serif;background-color:#F3F4F6">
-    <div style="background:#353988;padding:15px 0px;text-align:center">
-    <img src="https://res.cloudinary.com/drpi0krkh/image/upload/v1664196350/Logo_1_jhtgxs.png" alt=${logo} height="26" width="30" />
-  </div>
-    <div>
-    <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-    <td style="background-color:#353988;padding-right:20px;padding-left:20px">
-            <p style="text-align:center;font-size:20px;line-height:28px;color:#4B5563;background:#fff;margin:0 auto;padding:20px;max-width: 350px;padding-bottom:13px;"><span>Welcome !</span></p></tr>
-        </td>
-    </tr>
-  <tr>
-      <td align="center" style="padding: 0 20px">
-          <div style="background:#fff;margin:0 auto;padding:20px;max-width: 350px; padding-top: 0px;">
-        <div style="font-size:16px; line-height:24px;margin-bottom:16px">
-          <p style="text-align:left;color:#4B5563;margin:0;"><span>Hi,</span>
-          </p>
-          <br>
-          <p style="text-align:left;color:#4B5563;margin:0;">You have been invited to join k-Quiz by ${name}
-          </p>
-          <br>
-          <p style="text-align:left;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;">
-          Please use below link to join workspace
-          </p>
-          <br>
-        </div>        
-        <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;margin-top:10px">     
-          <a href=${workspaceJoinLink} style="color: #353988;">${workspaceJoinLink}</a>
-        </p>
-        <br>
-         <p style="text-align:center;color:#4B5563;margin:0;font-family:'Poppins', sans-serif;">
-             Use above link to generate your password
-          </p>
-      </div>
-      </td>
-  </tr>
-</table>
-      
-      <div style="text-align:center;padding: 20px 20px 24px;font-family:'Poppins', sans-serif;">K - Quiz @ Copods</div>
-    </div>
-    </div>
-  </body>
-</html>
-    `
+  const html = MEMBER_INVITE_TEMPLATE({ logo, name, workspaceJoinLink })
 
-  sendgrid.setApiKey(env.SENDGRID_API_KEY as string)
-
-  const msg = {
-    to,
-    from, // Use the email address or domain you verified above
-    subject,
-    text,
-    html,
+  try {
+    return await callSengridAPI({
+      to,
+      from, // Use the email address or domain you verified above
+      subject,
+      text,
+      html,
+    })
+  } catch (err) {
+    throw new Error(sendGridFailed)
   }
-  await sendgrid.send(msg).then(
-    () => {
-      return 'ok'
-    },
-    (error) => {
-      if (error.response) {
-        console.error('Sendgrid Error: ', error.response.body)
-      }
-    }
-  )
-
-  return 'Done'
 }
