e696d66a087295f9b8127166101eb98270a1bd65
Ayushi Jain <ayushi.jain@copods.co>
2023-02-03 14:22:46 +0530

    fix: styling of chip

--- app/components/tests/ChipGroup.tsx
+++ app/components/tests/ChipGroup.tsx
@@ -26,7 +26,7 @@ const ChipGroup = ({
 
   return (
     <div className="chip-group mr-3 flex items-center gap-2" ref={inputRef}>
-      <div className="truncate rounded-52 bg-blue-50 px-1 py-1">
+      <div className="truncate rounded-52 bg-blue-50 px-1.5 py-1.5 text-xs text-gray-900">
         {sections[0]?.section.name}
       </div>
       {sections.length > 1 && (
@@ -35,7 +35,7 @@ const ChipGroup = ({
             <Menu.Button onClick={() => setIsViewPortVisible(isInViewport())}>
               <div
                 aria-label={`+ ${sections.length - 1} Sections in Test`}
-                className="cursor-pointer rounded-52 bg-blue-50 px-1 py-1 text-xs"
+                className="cursor-pointer rounded-52 bg-blue-50 px-1 py-1 text-xs text-gray-900"
               >
                 + {sections.length - 1}
               </div>
@@ -54,22 +54,15 @@ const ChipGroup = ({
               id="menu-items"
               className={`sectionMenu absolute top-8 z-40 max-h-52 overflow-auto rounded-2xl bg-white py-4 px-4 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
                 isViewPortVisible ? '' : '-top-1 -translate-y-full transform'
-              }`}
+              } flex flex-col gap-4 border-gray-300 shadow-2xl`}
             >
               {sections.map((sect) => {
                 return (
-                  <div
-                    className="flex flex-col px-2 py-2"
-                    key={sect?.section?.id}
-                  >
-                    <Menu.Item>
-                      <span className="text-sm">
-                        <span className="truncate rounded-52 bg-blue-50 px-2 py-1">
-                          {sect?.section?.name}
-                        </span>
-                      </span>
-                    </Menu.Item>
-                  </div>
+                  <Menu.Item key={sect?.section?.id}>
+                    <span className="truncate rounded-52 bg-blue-50 px-2 py-1.5 text-xs text-gray-900">
+                      {sect?.section?.name}
+                    </span>
+                  </Menu.Item>
                 )
               })}
             </Menu.Items>
