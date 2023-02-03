7e359440b8a084cad0b7bc6ee265a0d081dd5d8f
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-02-03 23:14:08 +0530

    refactor: sign up tests

--- cypress/e2e/sign-up/SignUp.cy.ts
+++ cypress/e2e/sign-up/SignUp.cy.ts
@@ -5,34 +5,38 @@ import {
   getLastName,
   getPassword,
   getSignUpPageTitle,
+  getEmail,
+  getToastMessage,
 } from 'support/common-function'
 const signupPageTitle = 'Sign Up'
 const firstName = 'John'
 const lastName = 'Doe'
 const memberEmail = 'john@example.com'
 const memberAlreadyExist = 'Member with this email id already exists!'
-const signUp = 'Sign Up'
 
 describe('Test for Sign Up page', () => {
   beforeEach('/sign-up', () => {
     cy.visit('/sign-up')
   })
-  it('Checks for signup page title should have correct text and attributes', () => {
+
+  it('Tests to check Attributes/Colors/Visibility/Texts', () => {
+    // To check signup title has correct text and correct attributes
     getSignUpPageTitle()
       .should('have.text', signupPageTitle)
       .should('have.css', 'font-weight', '700')
       .should('have.css', 'font-size', '30px')
       .should('have.css', 'color', 'rgb(17, 24, 39)')
-  })
 
-  it('Checks for Sign Up CTA button text and atttributes', () => {
+    // To check signup CTA button has correct text and correct attributes
     getAddButton()
       .should('have.text', signupPageTitle)
       .should('have.css', 'background-color', 'rgb(162, 164, 214)')
       .should('have.css', 'padding', '10px 16px')
-  })
+      .should('have.css', 'color', 'rgb(249, 250, 251)')
+      .should('have.css', 'font-size', '12px')
+      .should('have.css', 'font-weight', '500')
 
-  it('Checks for Sign Up CTA button cursor', () => {
+    // To check signup CTA button cursor
     getFirstName()
       .should('be.visible')
       .clear()
@@ -43,7 +47,7 @@ describe('Test for Sign Up page', () => {
       .clear()
       .type(lastName)
       .should('have.value', lastName)
-    cy.get('#email')
+    getEmail()
       .should('be.visible')
       .clear()
       .type(memberEmail)
@@ -52,57 +56,25 @@ describe('Test for Sign Up page', () => {
     getPassword().should('be.visible').clear().type('newPassword')
     getConfirmPassword().should('be.visible').clear().type('newPassword')
     getAddButton().should('have.css', 'cursor', 'pointer')
-  })
-  it('Checks for Sign Up CTA button properties', () => {
-    getAddButton()
-      .should('have.css', 'color', 'rgb(249, 250, 251)')
-      .should('have.css', 'font-size', '12px')
-      .should('have.css', 'font-weight', '500')
-  })
-  it('trying to sign up successfully', () => {
-    getFirstName()
-      .should('be.visible', { timeout: 60000 })
-      .clear()
-      .type(firstName)
-      .should('have.value', firstName)
-    getLastName()
-      .should('be.visible', { timeout: 60000 })
-      .clear()
-      .type(lastName)
-      .should('have.value', lastName)
-    cy.get('#email')
-      .should('be.visible', { timeout: 60000 })
-      .clear()
-      .type(memberEmail)
-      .should('have.value', memberEmail)
 
-    getPassword().should('be.visible').clear().type('newPassword')
-    getConfirmPassword().should('be.visible').clear().type('newPassword')
-    getAddButton().should('have.text', signUp).click()
-  })
-  it('trying to sign up but member is already exist', () => {
-    getFirstName()
-      .should('be.visible', { timeout: 60000 })
-      .clear()
-      .type(firstName)
-      .should('have.value', firstName)
-    getLastName()
-      .should('be.visible', { timeout: 60000 })
-      .clear()
-      .type(lastName)
-      .should('have.value', lastName)
-    cy.get('#email')
-      .should('be.visible', { timeout: 60000 })
-      .clear()
-      .type(memberEmail)
-      .should('have.value', memberEmail)
+    // To check if signup is working
+    getFirstName().clear().type(firstName).should('have.value', firstName)
+    getLastName().clear().type(lastName).should('have.value', lastName)
+    getEmail().clear().type(memberEmail).should('have.value', memberEmail)
 
-    getPassword().should('be.visible').clear().type('newPassword')
-    getConfirmPassword().should('be.visible').clear().type('newPassword')
-    getAddButton().should('have.text', signUp).click()
-    cy.get('.Toastify__toast', { timeout: 8000 }).should(
-      'have.text',
-      memberAlreadyExist
-    )
+    getPassword().clear().type('newPassword')
+    getConfirmPassword().clear().type('newPassword')
+    getAddButton().click()
+
+    // To check member already exists toast
+    cy.visit('/sign-up')
+    getFirstName().type(firstName).should('have.value', firstName)
+    getLastName().type(lastName).should('have.value', lastName)
+    getEmail().type(memberEmail).should('have.value', memberEmail)
+
+    getPassword().clear().type('newPassword')
+    getConfirmPassword().clear().type('newPassword')
+    getAddButton().click()
+    getToastMessage().should('have.text', memberAlreadyExist)
   })
 })
