3e0d43f1ae8147c6cd336186938e7a2e07903e8d
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-02-04 10:36:35 +0530

    refactor: candidate list of assessment tests

--- cypress/e2e/group-by-test/CandidateListOfAssessment.cy.ts
+++ cypress/e2e/group-by-test/CandidateListOfAssessment.cy.ts
@@ -6,15 +6,12 @@ import {
 } from 'support/common-function'
 
 describe('Visiting group by test of results page', () => {
-  // creating data to test Test list page
-  beforeEach('sign-in', () => {
-    cy.login()
-
-    cy.customVisit('/members')
-  })
   const test1 = `Aptitude - assessment1`
 
   it('Checks,elements in candidate list', () => {
+    cy.login()
+    cy.customVisit('/members')
+
     getGroupByTestId().click()
     getGroupByItemTest().contains(test1).click()
 
@@ -22,9 +19,9 @@ describe('Visiting group by test of results page', () => {
     getTitle()
       .should('be.visible')
       .should('have.class', 'text-3xl font-semibold text-gray-900')
-    cy.wait(500)
+
     //checks button should be visible and after clicking it back to the result page
     getBackButton().should('be.visible').click()
-    cy.location('pathname', { timeout: 60000 }).should('include', '/results')
+    cy.location('pathname').should('include', '/results')
   })
 })
