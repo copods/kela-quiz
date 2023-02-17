119ef72ef1a1036d5782a50714ecc6a9549b4579
Apple <apple@Yash-Copods.local>
2023-02-17 11:45:56 +0530

    fix: chip padding

--- app/components/common-components/Chip.tsx
+++ app/components/common-components/Chip.tsx
@@ -6,14 +6,14 @@ const Chip = ({
   variant: string
 }) => {
   const variantMap: any = {
-    success: "bg-green-100 text-green-800 py-2 px-4",
-    error: "bg-red-100 text-red-800 py-2 px-4 ",
-    warning: "bg-yellow-100 text-yellow-800 py-2 px-4 ",
-    default: "border rounded-52 border-black text-grey-800 px-3 py-1",
+    success: "bg-green-100 text-green-800",
+    error: "bg-red-100 text-red-800",
+    warning: "bg-yellow-100 text-yellow-800",
+    default: "border rounded-52 border-black text-grey-800",
   }
   return (
     <span
-      className={`rounded-full text-xs font-medium ${variantMap[variant]}`}
+      className={`rounded-full px-3 py-1 text-xs font-medium ${variantMap[variant]}`}
       data-cy="chip-tag"
     >
       {text}
