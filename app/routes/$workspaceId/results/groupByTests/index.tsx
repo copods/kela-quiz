ed22aa5389a04dccdb20ef3ffcadc942f74ea1a4
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-03-06 10:38:20 +0530

    refactor: function naming

--- app/routes/$workspaceId/results/groupByTests/index.tsx
+++ app/routes/$workspaceId/results/groupByTests/index.tsx
@@ -7,7 +7,7 @@ import { sortByOrder } from "~/interface/Interface"
 import {
   getDetailsOfAllAssessments,
   getTotalAssessmentCount,
-  getTotalCountOfTests,
+  getTotalTestsCount,
   getWorkspaces,
 } from "~/services/results.service"
 import { getUserId } from "~/session.server"
@@ -35,7 +35,7 @@ export const loader: LoaderFunction = async ({ request, params }) => {
   const workspaces = await getWorkspaces(userId as string)
   if (!userId) return redirect("/sign-in")
   try {
-    const totalTestCount = await getTotalCountOfTests(
+    const totalTestCount = await getTotalTestsCount(
       params.testId!,
       currentWorkspaceId,
       userId
--- app/services/results.service.ts
+++ app/services/results.service.ts
@@ -67,7 +67,7 @@ export const getWorkspaces = async (userId: string) => {
  * @param userId
  * @returns total count
  */
-export const getTotalCountOfTests = async (
+export const getTotalTestsCount = async (
   id: string,
   workspaceId: string,
   userId: string
