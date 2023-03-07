d776c0e56d5fbcd85a44248269f5f78fedc74c4f
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-03-07 14:16:47 +0530

    refactor: test cases

--- cypress/e2e/group-by-test/resultDetails.cy.ts
+++ cypress/e2e/group-by-test/resultDetails.cy.ts
@@ -103,6 +103,20 @@ describe("result details", () => {
     //checks view detail button and click
     getResultDetails().eq(1).should("have.text", "View Details").click()
 
+    //checks correct answer chip class
+    getGivenAnswer()
+      .first()
+      .should("be.visible")
+      .should("have.class", "text-xl font-medium text-gray-800")
+      .should("have.text", "Given Answers")
+      .parent()
+      .within(() => {
+        getChipTag().should(
+          "have.class",
+          "rounded-full px-3 py-1 text-xs font-medium border border-green-100 bg-green-100 text-green-800"
+        )
+      })
+
     //checks wrong answer chip class
     getGivenAnswer()
       .eq(1)
