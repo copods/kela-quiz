2a6a488474cbc6634dc65560e88792adb7461495
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-03-13 18:58:48 +0530

    Revert "refactor: update test case"
    This reverts commit 543e5e018cc295b7a31ec02a5b2a393c846827bb.

--- cypress/e2e/tests/test-table.cy.ts
+++ cypress/e2e/tests/test-table.cy.ts
@@ -126,16 +126,6 @@ describe("Visiting Assessment", () => {
         }
       })
 
-    // To check sort by date in ascending
-    getSortFilterBody().get("#ascend").click()
-    sortFilterContainer().within(() => {
-      getDropdownButton()
-        .click({ multiple: true })
-        .get("li div")
-        .get(".dropdown-option")
-        .get(".not-selected")
-        .click()
-    })
     // To check sort by name in descending
     getSortFilterBody().get("#descend").click()
     cy.get(".dropdownButton span span", { timeout: 6000 })
@@ -149,6 +139,16 @@ describe("Visiting Assessment", () => {
         }
       })
 
+    // To check sort by date in ascending
+    getSortFilterBody().get("#ascend").click()
+    sortFilterContainer().within(() => {
+      getDropdownButton()
+        .click({ multiple: true })
+        .get("li div")
+        .get(".dropdown-option")
+        .get(".not-selected")
+        .click()
+    })
     cy.get(".dropdownButton span span", { timeout: 6000 })
       .invoke("text")
       .then((el) => {
