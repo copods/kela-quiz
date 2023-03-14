755ffee7258786af88ced80372a98ca953c7c1c0
Dhruv-Copods <dhruv.samant+1@copods.co>
2023-03-14 12:02:22 +0530

    refactor: updated test case

--- cypress/e2e/group-by-test/resultDetails.cy.ts
+++ cypress/e2e/group-by-test/resultDetails.cy.ts
@@ -108,7 +108,7 @@ describe("result details", () => {
       .first()
       .should("be.visible")
       .should("have.class", "text-xl font-medium text-gray-800")
-      .should("have.text", "Given Answer")
+      .should("have.text", "Given Answers")
       .parent()
       .within(() => {
         getChipTag().should(
@@ -122,7 +122,7 @@ describe("result details", () => {
       .eq(1)
       .should("be.visible")
       .should("have.class", "text-xl font-medium text-gray-800")
-      .should("have.text", "Given Answers")
+      .should("have.text", "Given Answer")
       .parent()
       .within(() => {
         getChipTag().should(
