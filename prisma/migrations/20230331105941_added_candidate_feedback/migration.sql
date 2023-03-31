8d7f09e8c1e5a38837adb08b9021fd143e6fccf2
Ayushi Jain <ayushi.jain@copods.co>
2023-03-31 16:35:16 +0530

    fix: added created at and updated at in schema

--- prisma/migrations/20230331105941_added_candidate_feedback/migration.sql
+++ prisma/migrations/20230331105941_added_candidate_feedback/migration.sql
@@ -3,6 +3,8 @@ CREATE TABLE "UserFeedback" (
     "id" TEXT NOT NULL,
     "candidateTestId" TEXT NOT NULL,
     "candidateId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "UserFeedback_pkey" PRIMARY KEY ("id")
 );
