c0ef4b9236214e785a06c04c2efd1233c5cfba04
Anurag <anurag@copods.co>
2024-10-22 11:27:22 +0530

    update

--- prisma/migrations/20221006123902_workspace_queries/migration.sql
+++ prisma/migrations/20221006123902_workspace_queries/migration.sql
@@ -1,4 +1,9 @@
-CREATE EXTENSION pgcrypto;
+DO $$ 
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_extension WHERE extname = 'pgcrypto') THEN
+        CREATE EXTENSION pgcrypto;
+    END IF;
+END $$;
 
 DO $$
 DECLARE 
